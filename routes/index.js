e52b2ac9d2c58d03de2e1e89284a302197e1deb9
Mauro <bono.mauro@gmail.com>
2018-04-08 16:08:43 +0200

    add store model and set slug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-//import storeController with the all different controller
+//import storeController with all the different controller
 const storeController = require('../controllers/storeController');
 
 //use the homePage controller
